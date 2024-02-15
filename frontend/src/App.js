39ba9879a889a864a7cf4ed6a3d85daf2b58ed84
Yuva sai Varma Jampana <yuvasaivarmaj@gmail.com>
2024-02-15 11:52:40 -0500

    changed back source path to localhost so that program can run a browser locally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://goals-backend/goals');
+        const response = await fetch('http://localhost/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://goals-backend/goals', {
+      const response = await fetch('http://localhost/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://goals-backend/goals/' + goalId, {
+      const response = await fetch('http://localhost/goals/' + goalId, {
         method: 'DELETE',
       });
 
