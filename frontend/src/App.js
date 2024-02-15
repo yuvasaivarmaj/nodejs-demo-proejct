0bb39a82ec5340a4ee5225668286988e9e6d4133
Yuva sai Varma Jampana <yuvasaivarmaj@gmail.com>
2024-02-15 11:29:02 -0500

    just changed connector path from localhost to internal container backend path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://goals-backend/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://goals-backend/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch('http://goals-backend/goals/' + goalId, {
         method: 'DELETE',
       });
 
